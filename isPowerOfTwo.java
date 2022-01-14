d61b4c3adb02e65919c7621624a9021e52e2ba4b
Juho Rantaharju <juho.rantaharju2@gmail.com>
2022-01-14 23:55:05 +0200

    one more excersice for fun

---
+++ isPowerOfTwo.java
@@ -0,0 +1,16 @@
+public class isPowerOfTwo {
+
+
+    public static void main(String[] args)
+    {
+
+        int isItPowerOfTwo = 1345;
+
+        if(isItPowerOfTwo%2==0)
+            System.out.println("It is power of two");
+        else
+        System.out.println("It is not power of two");
+        
+    }
+    
+}
